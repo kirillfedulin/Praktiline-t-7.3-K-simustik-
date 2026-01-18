263d9d2b42a9bd7f15126291f417b8b6704c30cf
Kirill Fedulin <crazzyhd22@gmail.com>
2026-01-18 14:01:26 +0200

    def load_questions

--- kusimustlik_funktsioon.py
+++ kusimustlik_funktsioon.py
@@ -1,9 +1,19 @@
 import os
 
-def load_questions(question_file="kusimused.txt", answer_file="vastused.txt"):
+def load_questions(questions_file="kusimused.txt", answers_file="vastused.txt"):
     kus_vas = {}
-    if not os.path.exsist(question_file) or not os.path.exsist(answer_file)
-        return kus_vas
+    if not os.path.exists(questions_file) or not os.path.exists(answers_file):
+    return kus_vas
     
-    with open(question_file, encoding="utf8") as file_question, open(answer_file, encoding="utf8") as file_answer:
-        
+    with open(question_file, encoding="utf8") as file_questions, open(answer_file, encoding="utf8") as file_answers:
+        questions = [line.strip() for line in file_questions if line.strip()]
+        answers - [line.strip() for line in file_answers if line.strip()]
+
+    if len(question) != len(answers):
+        print("kusimuste ja vastuse arv ei uhti!")
+        min_len = min(len(questions), len(answers))
+        question = question[:min_len]
+        answers = answers[:min_len]
+
+    kus_vas = dict(zip(questions, answers))
+    return kus_vas
